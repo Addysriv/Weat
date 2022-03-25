f9ecba37bf7fcbf600de38f635fbbdf70c3dc363
Addysriv <addysriv27@gmail.com>
2022-03-25 09:59:37 +0530

    folder structure change

---
+++ src/main/java/com/weat/weat/user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.weat.weat.user.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	Logger logger=LoggerFactory.getLogger(getClass());
+	
+	
+	
+	
+}
